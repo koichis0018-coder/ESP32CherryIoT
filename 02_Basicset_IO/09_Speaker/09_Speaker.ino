d9f834e3df75f47db2b830808c929abf75e72266
koichis0018-coder <koichi.s0018@gmail.com>
2026-04-09 13:37:03 +0900

    my original melody

--- 02_Basicset_IO/09_Speaker/09_Speaker.ino
+++ 02_Basicset_IO/09_Speaker/09_Speaker.ino
@@ -1,29 +1,29 @@
 const int spkrPin = 3; //3:ConnectorA 4:ConnectorB
 
-#define DO 261.6
-#define _DO 277.18
-#define RE 293.665
-#define _RE 311.127
-#define MI 329.63
-#define FA 349.228
-#define _FA 369.994
-#define SO 391.995
-#define _SO 415.305
-#define RA 440
-#define _RA 466.164
-#define TI 493.883
-#define octDO 523.251
+ #define DO 261.6
+ #define _DO 277.18
+ #define RE 293.665
+ #define _RE 311.127
+ #define MI 329.63
+ #define FA 349.228
+ #define _FA 369.994
+ #define SO 391.995
+ #define _SO 415.305
+ #define RA 440
+ #define _RA 466.164
+ #define TI 493.883
+ #define octDO 523.251
 
-void playmusic(){
-  ledcWriteTone(spkrPin, DO);
-  delay(250);
-  ledcWriteTone(spkrPin, RE);
-  delay(250);
-  ledcWriteTone(spkrPin, MI);
-  delay(250);
-  ledcWriteTone(spkrPin, 0); // no sound
-  delay(250);
-}
+// void playmusic(){
+//   ledcWriteTone(spkrPin, DO);
+//   delay(250);
+//   ledcWriteTone(spkrPin, RE);
+//   delay(250);
+//   ledcWriteTone(spkrPin, MI);
+//   delay(250);
+//   ledcWriteTone(spkrPin, 0); // no sound
+//   delay(250);
+// }
 
 void setup() {
   pinMode(spkrPin, OUTPUT);
@@ -32,5 +32,64 @@ void setup() {
 
 void loop() {
   playmusic();
-  delay(3000);
+  delay(650);
+}
+
+//麻原正晃のテーマソング、作りかけ
+void playmusic(){
+  ledcWriteTone(spkrPin, MI);
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+  ledcWriteTone(spkrPin, MI);
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+    ledcWriteTone(spkrPin, MI);
+  delay(150);
+  ledcWriteTone(spkrPin, DO);
+  delay(150);
+  ledcWriteTone(spkrPin, MI);
+  delay(150);
+  ledcWriteTone(spkrPin, DO);
+  delay(150);
+    ledcWriteTone(spkrPin, MI);
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+
+  ledcWriteTone(spkrPin, SO);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(250);  
+  ledcWriteTone(spkrPin, SO);
+  delay(250);
+  ledcWriteTone(spkrPin, FA);
+  delay(250);
+  ledcWriteTone(spkrPin, MI);
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(50);
+  
+
+  // ledcWriteTone(spkrPin, FA);
+  // delay(250);
+  // ledcWriteTone(spkrPin, SO);
+  // delay(250);
+  // ledcWriteTone(spkrPin, octDO);
+  // delay(250);  
+  // ledcWriteTone(spkrPin, DO);
+  // delay(250); 
+  // ledcWriteTone(spkrPin, SO);
+  // delay(250);
+  // ledcWriteTone(spkrPin, RA);
+  // delay(250); 
+  // ledcWriteTone(spkrPin, SO);
+  // delay(250); 
+  // ledcWriteTone(spkrPin, DO);
+  // delay(250);
+  // ledcWriteTone(spkrPin, FA);
+  // delay(250);
+  // ledcWriteTone(spkrPin, 0);
+  // delay(250);
 }
