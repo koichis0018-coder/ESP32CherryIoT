b030f64327a4a4e7c5770395c695a60c7d980cc2
koichis0018-coder <koichi.s0018@gmail.com>
2026-04-16 12:09:33 +0900

    change message

---
+++ 0416_sketch_apr16a/0416_sketch_apr16a.ino
@@ -0,0 +1,41 @@
+const int swPin = 3; //3:ConnectorA 4:ConnectorB
+const int spkrPin = 4; //3:ConnectorA 4:ConnectorB
+
+#define BEAT 230
+#define DO 261.6
+#define _DO 277.18
+#define RE 293.665
+#define _RE 311.127
+#define MI 329.63
+#define FA 349.228
+#define _FA 369.994
+#define SO 391.995
+#define _SO 415.305
+#define RA 440
+#define _RA 466.164
+#define TI 493.883
+#define octDO 523.251
+
+bool swState;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(swPin, INPUT);
+  pinMode(spkrPin, OUTPUT);
+}
+
+void loop() {
+
+  if (digitalRead(swPin) == HIGH) {
+    swState = !swState;
+
+    ledcWriteTone(spkrPin, DO);
+    delay(250);
+    
+  } else {
+    digitalWrite(motorPin, LOW); //OFF
+    Serial.println("OFF");
+    delay(300);
+  }
+  
+}
