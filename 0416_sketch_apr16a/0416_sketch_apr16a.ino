5e6a8c7c7f181ceadb8a71b505581105c7b0ca47
koichis0018-coder <koichi.s0018@gmail.com>
2026-04-16 14:07:43 +0900

    change message

--- 0416_sketch_apr16a/0416_sketch_apr16a.ino
+++ 0416_sketch_apr16a/0416_sketch_apr16a.ino
@@ -5,6 +5,12 @@ const int spkrPin = 4; //3:ConnectorA 4:ConnectorB
 #define HIGH_MI 1318.51  // 高いミ
 #define HIGH_FA 1396.91  // 高いファ
 
+#define H_RE   1174.66 // チ
+#define H_REs  1244.51 // (レ#) 緊張感を出すための半音
+#define H_FA   1396.91 // カ
+#define H_FAs  1479.98 // (ファ#) ンーー！のベース
+#define H_SO   1567.98 // (ソ) 
+
 bool swState;
 
 void setup() {
@@ -20,14 +26,14 @@ void loop() {
   if (digitalRead(swPin) == HIGH) {
     swState = !swState;
 
-  ledcWriteTone(spkrPin, HIGH_RE);
+  ledcWriteTone(spkrPin, H_REs);
   delay(150);
-  ledcWriteTone(spkrPin, HIGH_RE);
+  ledcWriteTone(spkrPin, H_REs);
   delay(150);
-  ledcWriteTone(spkrPin, HIGH_FA);
+  ledcWriteTone(spkrPin, H_FAs);
   delay(500);
-  ledcWriteTone(spkrPin, HIGH_RE);
-  delay(50);
+  //ledcWriteTone(spkrPin, HIGH_RE);
+  //delay(50);
 
     
   } else {
