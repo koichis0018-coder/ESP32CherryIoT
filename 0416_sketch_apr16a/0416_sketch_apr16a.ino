7d25d7ea6262d4a6fa815a109250e6ce2059d983
koichis0018-coder <koichi.s0018@gmail.com>
2026-04-16 13:51:51 +0900

    change message

--- 0416_sketch_apr16a/0416_sketch_apr16a.ino
+++ 0416_sketch_apr16a/0416_sketch_apr16a.ino
@@ -5,21 +5,6 @@ const int spkrPin = 4; //3:ConnectorA 4:ConnectorB
 #define HIGH_MI 1318.51  // 高いミ
 #define HIGH_FA 1396.91  // 高いファ
 
-#define BEAT 230
-#define DO 261.6
-#define _DO 277.18
-#define RE 293.665
-#define _RE 311.127
-#define MI 329.63
-#define FA 349.228
-#define _FA 369.994
-#define SO 391.995
-#define _SO 415.305
-#define RA 440
-#define _RA 466.164
-#define TI 493.883
-#define octDO 523.251
-
 bool swState;
 
 void setup() {
@@ -40,7 +25,10 @@ void loop() {
   ledcWriteTone(spkrPin, HIGH_RE);
   delay(150);
   ledcWriteTone(spkrPin, HIGH_FA);
-  delay(1000);
+  delay(500);
+  ledcWriteTone(spkrPin, HIGH_RE);
+  delay(50);
+
     
   } else {
     digitalWrite(spkrPin, LOW); //OFF
