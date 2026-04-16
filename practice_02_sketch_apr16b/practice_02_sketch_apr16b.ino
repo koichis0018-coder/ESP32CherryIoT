007f9114acd12504e5ac27ce68b6918818ab6237
koichis0018-coder <koichi.s0018@gmail.com>
2026-04-16 10:12:09 +0900

    change code

--- practice_02_sketch_apr16b/practice_02_sketch_apr16b.ino
+++ practice_02_sketch_apr16b/practice_02_sketch_apr16b.ino
@@ -1,9 +1,30 @@
+const int swPin = 3; //3:ConnectorA 4:ConnectorB
+const int motorPin = 4; //3:ConnectorA 4:ConnectorB
+
+bool swState;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(11520);
+  pinMode(swPin, INPUT);
+  pinMode(motorPin, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (digitalRead(swPin) == HIGH) {
+    swState = !swState;
+  }
+
+  if (swState == 1) { 
+    digitalWrite(motorPin, HIGH); //ON
+    Serial.println("ON");
+    delay(300);
+  } else {
+    digitalWrite(motorPin, LOW); //OFF
+    Serial.println("OFF");
+    delay(300);
+  }
 
 }
