7a335ccbe2a02fa5deb6957f36fd0c51d50907ca
koichis0018-coder <koichi.s0018@gmail.com>
2026-05-21 16:04:51 +0900

    my original melody

--- 02_Basicset_IO/04_Temp-Humidity/04_Temp-Humidity.ino
+++ 02_Basicset_IO/04_Temp-Humidity/04_Temp-Humidity.ino
@@ -1,20 +1,32 @@
 #include <DHT20.h> //DHT20 by RobTillaart
 #include <Wire.h>
 
-DHT20 DHT;
-
 void setup() {
   Serial.begin(115200);
-  Wire.begin(1, 3); //1,3:ConnectorA 5,4:ConnectorB
+  pinMode(litsnsrPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  DHT.read();
-  float Temperature = DHT.getTemperature();
-  float Humidity = DHT.getHumidity();
+  float litsnsr_ad = analogRead(litsnsrPin); // Read analog data
+  float litsnsr_v = litsnsr_ad * 3.3 / 4096; // Calculation of voltage value
+  float lux = 10000 * litsnsr_v / (3.3 - litsnsr_v) / 1000; // Calculation of lux value
+
   //Look at the serial monitor
-  Serial.print(String(Temperature) + "℃");
-  Serial.print(" / ");
-  Serial.println(String(Humidity) + "％");
-  delay(500);
+  Serial.print(lux);
+  Serial.print(" Lux : ");
+  
+  // Change the threshold to suit your environment
+  if (lux > 20) { 
+    digitalWrite(ledPin, LOW); //OFF
+    Serial.println("Bright");
+    delay(500);
+  } else {
+    digitalWrite(ledPin, HIGH); //ON
+    Serial.println("Dark");
+    delay(500);
+  }
 }
+
+
+
